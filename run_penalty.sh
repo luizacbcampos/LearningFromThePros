130f0849374cdd99bf728181665c37393cb0e3c7
luizacbcampos <luizacbcampos@gmail.com>
2022-06-19 22:17:38 -0300

    penalty run

---
+++ run_penalty.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+view_invariant2=(1 0)
+penalty_location=(0 1)
+hand=(0 1)
+height=(0 1)
+
+# Iterate the string array using for loop
+for v2 in ${view_invariant2[@]}; do
+   for p in ${penalty_location[*]}; do
+       for h in ${hand[*]}; do
+           for hei in ${height[*]}; do
+               echo "python main.py -v2 $v2 -p $p -ha $h -he $hei"
+               # python main.py -v2 $v2 -p $p -ha $h -he $hei
+           done
+       done
+   done
+done
